32c6035947ecf4b9cd404446399bbb9dbeb6f198
Talha Korkmaz <148969705+talha667ko@users.noreply.github.com>
2023-11-11 09:12:28 +0300

    quiz1.c

---
+++ QUIZLER/quiz1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Talha KORKMAZ 230202010
+
+int main()
+{
+
+    char a = 'A';
+    char b = 'A';
+    char c = 'B';
+    char f = 'A';
+    int d = 1;
+    int e = 2;
+    
+    printf("%c%c\n%c%c\n%d%d", a, b, c, f, d, e);
+    
+    char x = 'Z';
+    char y = 'z';
+    float z = 12.36;
+    int m = 8;
+    
+    printf("\n%c%c %.2f %d\n", x, y, z, m);
+    
+    
+    return 0;
+
+    }
